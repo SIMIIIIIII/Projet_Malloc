9f023dbd751e3ac16068e633e1af5e7281dcb97f
JunoUCL <juno.ryckmans@student.uclouvain.be>
2025-10-19 12:08:31 +0200

    malloc3
    documents pour la réécriture du code

---
+++ malloc3.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "malloc3.h"
+
+///////////////////////////////////////
+//   FONCTIONS CRUCIALES             //
+//   à copier-coller dans Inginious  //
+///////////////////////////////////////
+
+void init(){
+    //TODO
+}
+
+void *my_malloc(size_t size){
+    //TODO
+}
+
+void my_free(void *pointer){
+    //TODO
+}
---
+++ test3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <time.h>
+#include "malloc3.h" 
+
+uint8_t MY_HEAP[SIZE_HEAP];
+
+
+
+int main(void) {
+
+    printf("\nTests de performance : \n");
+    
+    printf("Tests terminés! 🎉\n");
+    return 0;
+}
+
+//taper : gcc -Wall -Wextra -std=c11 test3.c malloc3.c -o test3
+//et puis : ./test3
